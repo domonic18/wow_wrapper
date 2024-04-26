5d616b26046ce4d6f972ec2fd5dccc4587e8cb63
Deawalk <zgdmemail@gmail.com>
2024-04-26 15:03:07 +0800

    初始化项目代码

---
+++ api/lib/HonorTrait.php
@@ -0,0 +1,67 @@
+<?php
+
+    trait HonorTrait
+    {
+        protected function fetchTopHonorPlayersFromMySQL() {
+            $db = $this->Connect(self::$_conf['characters']);
+
+            $sql = "
+                SELECT 
+                    c.guid, 
+                    c.name, 
+                    c.race, 
+                    c.class, 
+                    c.gender, 
+                    c.totalHonorPoints
+                FROM 
+                    characters AS c
+                ORDER BY totalHonorPoints DESC
+                LIMIT 50
+            ";
+
+            $result = $db->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+
+            return $result;
+        }
+
+        protected function processTopHonorPlayersData($topPlayersData) {
+            if ($topPlayersData !== false && count($topPlayersData) > 0) {
+                $topPlayers = array();
+                foreach ($topPlayersData as $key => $player) {
+                    $class = $this->ClassState($player['class']);
+                    $topPlayers[$key] = array(
+                        'Guid' => $player['guid'],
+                        'Name' => $this->checkAndReplacePlayerName($player['name']),
+                        'Race' => self::$_conf['site_url'] . $this->OnlineIcon((int)$player['race'], (int)$player['gender']),
+                        'Class' => $class['class'],
+                        'Gender' => ((int)$player['gender']),
+                        'TotalHonorPoints' => $player['totalHonorPoints']
+                    );
+                }
+
+                return $topPlayers;
+            } else {
+                return array();
+            }
+        }
+
+        public function getTopHonorPlayers() {
+            try {
+                $cacheKey = 'top_honor_players';
+
+                $topPlayers = $this->getDataFromRedis($cacheKey);
+
+                if ($topPlayers === null) {
+                    $topPlayersData = $this->fetchTopHonorPlayersFromMySQL();
+                    $topPlayers = $this->processTopHonorPlayersData($topPlayersData);
+
+                    $this->storeDataInRedis($cacheKey, $topPlayers);
+                }
+
+                $this->returnJsonResponse(true, $topPlayers);
+            } catch (PDOException $e) {
+                $this->returnJsonResponse(false, null, $e->getMessage());
+            }
+        }
+
+    }
